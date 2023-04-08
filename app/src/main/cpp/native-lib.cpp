f531b97a778fe5538d3eb5e2f2d8ae4664d88244
baiyang <17801003317@163.com>
2023-04-09 03:20:37 +0800

    SurfaceView、读取手机文件的权限

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,14 +2,44 @@
 #include <string>
 
 extern "C" {
+#include "include/libavcodec/avcodec.h"
 #include "include/libavutil/avutil.h"
+#include "include/libavfilter/avfilter.h"
+#include "include/libavformat/avformat.h"
+#include "include/libswresample/swresample.h"
+#include "include/libswscale/swscale.h"
 }
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_andplayer_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++\nFFmpeg version: ";
-    hello += av_version_info();
-    return env->NewStringUTF(hello.c_str());
+#include <android/log.h>
+#include <android/native_window_jni.h>
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_INFO,"baiyang",__VA_ARGS__)
+
+static AVFormatContext *avFormatContext;
+static AVCodecContext *avCodecContext;
+AVCodec *vCodec;
+ANativeWindow* nativeWindow;
+ANativeWindow_Buffer windowBuffer;
+
+static AVPacket *avPacket;
+static AVFrame *avFrame, *rgbFrame;
+struct SwsContext *swsContext;
+uint8_t *outbuffer;
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_example_andplayer_MainActivity_play(JNIEnv *env, jobject thiz, jstring url_,
+                                              jobject surface) {
+    const char *url = env->GetStringUTFChars(url_, 0);
+    avFormatContext = avformat_alloc_context();
+    //打开文件
+    if(avformat_open_input(&avFormatContext, url, NULL, NULL) != 0){
+        LOGD("Couldn't open input stream.\n");
+        return -1;
+    }
+    LOGD("打开视频成功.\n");
+
+    avformat_free_context(avFormatContext);
+    env->ReleaseStringUTFChars(url_, url);
+    return 0;
 }
--- app/src/main/java/com/example/andplayer/MainActivity.java
+++ app/src/main/java/com/example/andplayer/MainActivity.java
@@ -2,35 +2,65 @@ package com.example.andplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.os.Environment;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.media.MediaCodec;
 
-import com.example.andplayer.databinding.ActivityMainBinding;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-
     // Used to load the 'andplayer' library on application startup.
     static {
         System.loadLibrary("andplayer");
     }
-
-    private ActivityMainBinding binding;
+    Surface surface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
+//        MediaCodec mediaCodec= MediaCodec.createByCodecName("video/avc");
+//        mediaCodec.start();
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface);
+        final SurfaceHolder surfaceViewHolder = surfaceView.getHolder();
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        surfaceViewHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                //获取文件路径，这里将文件放置在手机根目录下(sdcard)
+                surface = surfaceViewHolder.getSurface();
+            }
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {    }
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {    }
+        });
+    }
+    public boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+            }, 1);
+        }
+        return false;
+    }
+    public void play(View view) {
+        String folderurl = new File(Environment.getExternalStorageDirectory(), "brave_960x540.flv").getAbsolutePath();
+        play(folderurl, surface);
     }
 
-    /**
-     * A native method that is implemented by the 'andplayer' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+
+    public native int play(String url, Surface surface);
 }
